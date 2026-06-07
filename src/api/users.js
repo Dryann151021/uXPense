1c6260a1fb735717af76aec1de6257e756123d09
adriyan <adrianadrian.aa613@gmail.com>
2026-06-07 12:18:05 +0700

    feat: add client api

---
+++ src/api/users.js
@@ -0,0 +1,18 @@
+import client from './client.js';
+
+export const usersApi = {
+  getMe: async () => {
+    const response = await client.get('/users');
+    return response.data.data;
+  },
+
+  getAll: async () => {
+    const response = await client.get('/users');
+    return response.data.data;
+  },
+
+  getById: async (id) => {
+    const response = await client.get(`/users/${id}`);
+    return response.data.data;
+  },
+};
